26d6a4e1a900f84633201a71f86841edc096be8b
ChenFangM <ChenFang.NY@gmail.com>
2023-04-02 23:30:48 -0400

    app

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    print("the __name__ of this module is... ")
-    print(__name__)
     return "No hablo queso!"
+
+@app.route("/tedxsd") 
+def test_tmplt():
+    return render_template('tedxsd.html') 
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
